a1a7dc73e90dfdb2ff0fb38acdb71b7f108f04da
ptg <749106618@qq.com>
2024-11-14 17:02:44 +0800

    feat: 环境搭建

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+@import 'normalize.css';
+#app {
+  /** 字体抗锯齿，让字体看起来更清晰 */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 14px;
+  color: #2c3e50;
+}
+</style>
